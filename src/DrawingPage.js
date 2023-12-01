2f30ea37324ae1b8e93b14cb9c4b0a78420bebca
Catherine Pan <pancathe@usc.edu>
2023-11-30 22:05:22 -0800

    attempted to start drawing code

--- src/DrawingPage.js
+++ src/DrawingPage.js
@@ -1,4 +1,4 @@
-import {React, useEffect} from 'react';
+import {React, useEffect, useRef} from 'react';
 import { Link } from 'react-router-dom';
 import { RiHome2Fill  } from 'react-icons/ri'; // Import the home icon]
 
@@ -7,6 +7,146 @@ import * as d3 from 'd3';
 import { starData } from "./starCatalog";
 
 function DrawingPage() {
+  const canvRef = useRef(null);
+
+  //mouse stuff
+  let prevX1 = null, prevY1 = null, prevX2 = null, prevY2 = null;
+  let mouseDown = false;
+  let wait = false;
+
+  //points
+  const points = []; //FILL WITH STARS
+  let highlighted = [];
+
+  //drawing points
+  let curComponent = -1;
+  let drawingPts = [];
+
+  //canvas drawing functions
+  function plotPoint(ctx, x, y, col="black", s=2){
+    ctx.fillStyle = col;
+    ctx.fillRect(x - s/2, y - s/2, s, s);
+  }
+  function plotPath(ctx, pts, col="black", strokeWidth=1){
+    if(pts.length === 0){
+      return;
+    }
+    ctx.beginPath();
+    ctx.fillStyle = "transparent";
+    ctx.strokeStyle = col;
+    ctx.lineWidth = strokeWidth;
+    for(let i = 0; i < pts.length; i++){
+      ctx.moveTo(pts[i][0].x, pts[i][0].y);
+      for(let j = 1; j < pts[i].length; j++){
+        ctx.lineTo(pts[i][j].x, pts[i][j].y);
+      }
+    }
+    ctx.stroke();
+    ctx.closePath();
+  }
+  function drawCanvas(canvas, ctx){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //drawing points
+    for(let i = 0; i < points.length; i++){
+      if(highlighted.indexOf(points[i]) !== -1){
+        plotPoint(ctx, points[i].x, points[i].y, "red", 4);
+      }
+      else {
+        plotPoint(ctx, points[i].x, points[i].y, "blue");
+      }
+    }
+    
+    //getting drawing?
+    plotPath(ctx, drawingPts);
+  }
+
+  function sqDist(x1, y1, x2, y2){
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+  }
+  function directionChange(prevX2, prevY2, prevX1, prevY1, curX, curY){
+    let pathLen = Math.sqrt(sqDist(prevX1, prevY1, curX, curY)) + Math.sqrt(sqDist(prevX2, prevY2, prevX1, prevY1));
+    let straightPath = Math.sqrt(sqDist(prevX2, prevY2, curX, curY));
+    return pathLen/straightPath;
+  }
+  function updateHighlight(mouseX, mouseY, threshold){
+    let minDist = Infinity;
+    let minInd = -1;
+    for(let i = 0; i < points.length; i++){
+      let dist = sqDist(points[i].x, points[i].y, mouseX, mouseY);
+      if(dist < threshold && dist < minDist){
+        minInd = i;
+        minDist = dist;
+      }
+    }
+    const lastPt = highlighted.length === 0 ? null:highlighted[highlighted.length - 1];
+    if(minInd === -1){
+      return;
+    }
+    if(lastPt === null || sqDist(lastPt.x, lastPt.y, points[minInd].x, points[minInd].y) > threshold * 4 || minDist < 100){
+      highlighted.push(points[minInd]);
+    }
+  }
+
+  //handling drag for users
+  function updateOnDrag(e, canvas, ctx){
+    //updating highlighted points
+    let factor = 1.2;
+    if(drawingPts[curComponent].length >= 2){
+      factor = directionChange(prevX2, prevY2, prevX1, prevY1, e.offsetX, e.offsetY);
+    }
+    let threshold = Math.max(225, 225 * factor * factor);
+    updateHighlight(e.offsetX, e.offsetY, threshold);
+
+    //adding points
+    drawingPts[curComponent].push({x : e.offsetX, y: e.offsetY});
+
+    //draw updated canvas
+    drawCanvas(canvas, ctx);
+
+    //updating stuff
+    prevX2 = prevX1;
+    prevY2 = prevY1;
+    prevX1 = e.offsetX;
+    prevY1 = e.offsetY;
+  }
+
+  //reset drawing
+  function reset(){
+    prevX2 = null;
+    prevY2 = null;
+    prevX1 = null;
+    prevY1 = null;
+
+    highlighted = [];
+    drawingPts = [];
+    curComponent = -1;
+    mouseDown = false;
+    wait = false;
+  }
+
+  //functions handling mouse movement
+  function handleMouseUp(){
+    mouseDown = false;
+    wait = false;
+  }
+  function handleMouseDown(){
+    mouseDown = true;
+    curComponent++;
+    drawingPts.push([]);
+  }
+  function handleMouseMove(e, canvas, ctx){
+    if(!mouseDown){
+      return;
+    }
+    if(!wait){
+      updateOnDrag(e, canvas, ctx);
+      wait = true;
+    }
+    if((prevX1 === null) || sqDist(prevX1, prevY1, e.offsetX, e.offsetY) >= 36){
+      wait = false;
+    }
+  }
+
   useEffect(() => {
     const container = document.getElementsByClassName('star-bg')[0];
 
@@ -30,7 +170,29 @@ function DrawingPage() {
         star.style.top = `${Math.random() * 96 + 2}%`;
         container.appendChild(star);
     }
+
+    //canvas
+    const canv = canvRef.current;
+    const ctx = canv.getContext("2d");
+
+    canv.onmousedown = handleMouseDown;
+    canv.onmouseup = handleMouseUp;
+    canv.onmouseleave = handleMouseUp;
+    const moveHandler = function(e){
+      handleMouseMove(e, canv, ctx);
+    }
+    canv.addEventListener("mousemove", moveHandler);
+    drawCanvas(canv, ctx);
+
+    //clean up event listeners
+    return () => {
+      canv.removeEventListener("mousedown", handleMouseDown);
+      canv.removeEventListener("mouseup", handleMouseUp);
+      canv.removeEventListener("mouseleave", handleMouseUp);
+      canv.removeEventListener("mousemove", moveHandler);
+    };
   }, []);
+
   return (
     <div className='drawing-container star-bg'>
       <Link to="/" className="home-link">
@@ -46,7 +208,7 @@ function DrawingPage() {
                 <span>Map to sky</span>
           </Link>
         </div>
-        <canvas className='drawing-whiteboard'>
+        <canvas className='drawing-whiteboard' ref = {canvRef}>
 
         </canvas>
       </div>
